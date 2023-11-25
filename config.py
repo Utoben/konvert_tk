3a2965eb627fd73a880359322d0796f6cb33f588
Utoben <ubelousov15@gmail.com>
2023-11-26 00:33:18 +0500

    Пример конвертера для учеников Tk, requests

---
+++ config.py
@@ -0,0 +1,1 @@
+API_KEY = 'lOmNJehjGlzCBAIkgabfM09sG9PpsPVH'
---
+++ konvertor.py
@@ -0,0 +1,117 @@
+from tkinter import *
+from tkinter import ttk
+import requests
+from config import *
+
+
+headers= {
+  "apikey": API_KEY,
+}
+
+uah = 'UAH'
+rub = 'RUB'
+eur = 'EUR'
+usd = 'USD'
+
+# создаем url запроса на сервер с нужными нам валютами
+def make_url(currency_to, currency_from, amount):
+	return f"https://api.apilayer.com/currency_data/convert?to={currency_to}&from={currency_from}&amount={amount}"
+
+# отправялем GET запрос на сервер и обрабатываем его ответ, извлекаем результат
+def get_konvert(to):
+		response=requests.get(to, headers=headers)
+		if response.status_code == 200:
+			result_json = response.json()
+			result=result_json.get("result")
+			res=round(result,3)
+			print(f'Результат конвертации: {res}')
+			return res
+		else:
+			print(f'Проблема: {response.status_code}')
+			return 0
+
+def konvert():
+	val2.delete(0, END)
+	a=float( val1.get() ) #получаем значение из поля1 
+	b=com.get() #первый комбобокс
+	d=com2.get() #второй комбобокс
+
+	# url для каждого уникального запроса. make_url(валюта_в_которую_конвертим, валюта_из_котор_конертим, количество)
+	eur_to_usd = make_url(usd, eur, a)
+	eur_to_rub = make_url(rub, eur, a)
+	eur_to_uah = make_url(uah, eur, a)
+	
+	usd_to_eur = make_url(eur, usd, a)
+	usd_to_uah = make_url(uah, usd, a)
+	usd_to_rub = make_url(rub, usd, a)
+
+	rub_to_eur = make_url(eur, rub, a)
+	rub_to_uah = make_url(uah, rub, a)
+	rub_to_usd = make_url(usd, rub, a)
+
+	uah_to_eur = make_url(eur, uah, a)
+	uah_to_rub = make_url(rub, uah, a)
+	uah_to_usd = make_url(usd, uah, a)
+		
+	if b=='EUR' and d!='EUR': # если конвертим из EUR
+		if d=='USD': result = get_konvert(eur_to_usd)
+		if d=='UAN': result = get_konvert(eur_to_uah)
+		if d=='RUB': result = get_konvert(eur_to_rub)
+
+	if b=='USD' and d!='USD': # если конвертим из USD
+		if d=='EUR': result = get_konvert(usd_to_eur)
+		if d=='UAN': result = get_konvert(usd_to_uah)
+		if d=='RUB': result = get_konvert(usd_to_rub)
+		
+	if b=='UAN' and d!='UAN': # если конвертим из UAN
+		if d=='USD': result = get_konvert(uah_to_usd)
+		if d=='EUR': result = get_konvert(uah_to_eur)
+		if d=='RUB': result = get_konvert(uah_to_rub)
+
+	if b=='RUB' and d!='RUB': # если конвертим из RUB
+		if d=='USD': result = get_konvert(rub_to_usd)
+		if d=='EUR': result = get_konvert(rub_to_eur)
+		if d=='UAN': result = get_konvert(rub_to_uah)
+
+	val2.insert(0 , result) #выводим результат в поле 2
+
+def center(win):
+	win.update_idletasks()
+	width = win.winfo_width()
+	height = win.winfo_height()
+	x = (win.winfo_screenwidth() // 2) - (width // 2)
+	y = (win.winfo_screenheight() // 2) - (height // 2)
+	win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+window = Tk() #создание окна
+window.title('Konvert') #изменяем заголовок
+window.geometry('500x300')
+center(window)
+window.resizable(False, False)
+
+USD=73.0 # курс доллара
+EUR=90.1 # курс евро
+UAN=11.4 # курс юань
+RUB=1    # рубль
+VAL=['USD','EUR','UAN' ,'RUB'] #список валют
+
+Label(window, text='Конвертор валют' , font='Times 30').pack()
+Label(window, text='Сумма:',   font='Times 15').place(x=120, y=84)
+Label(window, text='Конвертировать в:', font='Times 15').place(x=120, y=150)
+Label(window, text='Результат :', font='Times 15').place(x=120, y=200)
+Button(window , text='Провести конверт', padx='20', pady='8', bg='#32a893', command=konvert).place(x=200 , y = 250)
+#комбобокс 1
+com=ttk.Combobox(window, width=10, values=VAL , font='20' ,state='readonly')
+com.current(0)
+com.place(x=300, y =88)
+#поле для ввода 1
+val1=Entry(window, width=10)
+val1.place(x=200 , y=90)
+#комбобокс 2
+com2=ttk.Combobox(window, width=10, values=VAL , font='20' ,state='readonly')
+com2.current(0)
+com2.place(x=300, y =150)
+#поле для ввода 2
+val2=Entry(window, width=10)
+val2.place(x=230 , y=204)
+window.mainloop() #отобразить окно
